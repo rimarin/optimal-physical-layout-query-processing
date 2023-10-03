a534997fe6c4427ea0b4ed63a788e1bf1bea5e46
Riccardo Marin <riccardomarin23@gmail.com>
2023-10-03 13:33:14 +0200

    refactor: Update output paths
    Capitalize output path of partitioned data

--- test/main.cpp
+++ test/main.cpp
@@ -12,12 +12,12 @@ int main() {
     arrow::Result<std::shared_ptr<arrow::Table>> exampleTable = storage::DataWriter::GenerateExampleTable().ValueOrDie();
     std::string tableName = "test";
 
-    std::string noPartitionFolder = "test/noPartition/";
+    std::string noPartitionFolder = "test/NoPartition/";
     std::filesystem::path outputFolder = std::filesystem::current_path().parent_path() / noPartitionFolder;
     std::shared_ptr<partitioning::MultiDimensionalPartitioning> noPartitioning = std::make_shared<partitioning::NoPartitioning>();
     arrow::Status statusNoPartition = storage::DataWriter::WriteTable(*exampleTable, tableName, outputFolder, noPartitioning);
 
-    std::string fixedGridFolder = "test/fixedGrid/";
+    std::string fixedGridFolder = "test/FixedGrid/";
     outputFolder = std::filesystem::current_path().parent_path() / fixedGridFolder;
     std::vector<std::string> partitioningColumns = {"Day", "Month"};
     std::shared_ptr<partitioning::MultiDimensionalPartitioning> fixedGridPartitioning = std::make_shared<partitioning::FixedGrid>(partitioningColumns, 20);
