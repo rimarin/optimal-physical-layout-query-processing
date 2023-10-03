889fb72c4915a08b84554b4cc39af547be850825
Riccardo Marin <riccardomarin23@gmail.com>
2023-10-03 11:06:46 +0200

    refactor: refactor main
    Update lib paths, rename variables

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,22 @@
 
 #include <arrow/io/api.h>
 
-#include "../include/partitioning/FixedGrid.h"
-#include "partitioning/FixedGrid.cpp"
-#include "../include/storage/DataWriter.h"
-#include "storage/DataWriter.cpp"
+#include "partitioning/FixedGrid.h"
+#include "storage/DataWriter.h"
 
 
 int main() {
-    auto dataWriter = storage::DataWriter();
-    arrow::Result<std::shared_ptr<arrow::Table>> exampleTable = dataWriter.GenerateExampleTable();
+    arrow::Result<std::shared_ptr<arrow::Table>> exampleTable = storage::DataWriter::GenerateExampleTable();
     std::string tableName = "test";
 
     std::string noPartitionFolder = "test/noPartition/";
     std::filesystem::path outputFolder = std::filesystem::current_path().parent_path() / noPartitionFolder;
-    // arrow::Status statusNoPartition = dataWriter.WriteTable(*exampleTable, tableName, outputFolder, nullptr);
+    arrow::Status statusNoPartition = storage::DataWriter::WriteTable(*exampleTable, tableName, outputFolder, nullptr);
 
     std::string fixedGridFolder = "test/fixedGrid/";
     outputFolder = std::filesystem::current_path().parent_path() / fixedGridFolder;
     std::vector<std::string> partitioningColumns = {"Day", "Month"};
-    std::shared_ptr<partitioning::MultiDimensionalPartitioning> partitioningMethod = std::make_shared<partitioning::FixedGrid>(partitioningColumns, 2);
-    arrow::Status statusFixedGrid = dataWriter.WriteTable(*exampleTable, tableName, outputFolder,
-                                                          partitioningMethod);
+    std::shared_ptr<partitioning::MultiDimensionalPartitioning> fixedGridPartitioning = std::make_shared<partitioning::FixedGrid>(partitioningColumns, 2);
+    arrow::Status statusFixedGrid = storage::DataWriter::WriteTable(*exampleTable, tableName, outputFolder,fixedGridPartitioning);
     return 0;
 }
