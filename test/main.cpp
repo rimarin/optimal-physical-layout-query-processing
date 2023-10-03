565028ad13a2b9751a1b29efc7f1ed9a67858bd2
Riccardo Marin <riccardomarin23@gmail.com>
2023-10-03 11:58:44 +0200

    build: Move main file to tests folder
    Main implementation is now in tests

--- test/main.cpp
+++ test/main.cpp
@@ -4,16 +4,18 @@
 #include <arrow/io/api.h>
 
 #include "partitioning/FixedGrid.h"
+#include "partitioning/NoPartitioning.h"
 #include "storage/DataWriter.h"
 
 
 int main() {
-    arrow::Result<std::shared_ptr<arrow::Table>> exampleTable = storage::DataWriter::GenerateExampleTable();
+    arrow::Result<std::shared_ptr<arrow::Table>> exampleTable = storage::DataWriter::GenerateExampleTable().ValueOrDie();
     std::string tableName = "test";
 
     std::string noPartitionFolder = "test/noPartition/";
     std::filesystem::path outputFolder = std::filesystem::current_path().parent_path() / noPartitionFolder;
-    arrow::Status statusNoPartition = storage::DataWriter::WriteTable(*exampleTable, tableName, outputFolder, nullptr);
+    std::shared_ptr<partitioning::MultiDimensionalPartitioning> noPartitioning = std::make_shared<partitioning::NoPartitioning>();
+    arrow::Status statusNoPartition = storage::DataWriter::WriteTable(*exampleTable, tableName, outputFolder, noPartitioning);
 
     std::string fixedGridFolder = "test/fixedGrid/";
     outputFolder = std::filesystem::current_path().parent_path() / fixedGridFolder;
