5f5f036cabbba2e8d67f8de724aacb729cbbaf8e
Riccardo Marin <riccardomarin23@gmail.com>
2023-10-14 15:26:55 +0200

    feat: Functions for example table generation
    Add functions to write to disk the example table and the kdtree example table.

--- test/main.cpp
+++ test/main.cpp
@@ -10,11 +10,31 @@
 #include "storage/DataReader.h"
 
 
-int main() {
+void generate_test_kdtree_file(){
+    arrow::Result<std::shared_ptr<arrow::Table>> kdTreeExampleTable = storage::DataWriter::GenerateKDTreeExampleTable().ValueOrDie();
+    std::string kdTreeTableName = "kdTree";
+    std::string noPartitionFolder = "test/NoPartition/";
+    std::filesystem::path outputFolder = std::filesystem::current_path().parent_path() / noPartitionFolder;
+    std::shared_ptr<partitioning::MultiDimensionalPartitioning> noPartitioning = std::make_shared<partitioning::NoPartitioning>();
+    arrow::Status statusNoPartitionKDTree = storage::DataWriter::WriteTable(*kdTreeExampleTable, kdTreeTableName, outputFolder, noPartitioning);
+}
 
+void generate_test_parquet_file(){
+    arrow::Result<std::shared_ptr<arrow::Table>> exampleTable = storage::DataWriter::GenerateExampleTable().ValueOrDie();
+    std::string exampleTableName = "test";
+    std::string noPartitionFolder = "test/NoPartition/";
+    std::filesystem::path outputFolder = std::filesystem::current_path().parent_path() / noPartitionFolder;
+    std::shared_ptr<partitioning::MultiDimensionalPartitioning> noPartitioning = std::make_shared<partitioning::NoPartitioning>();
+    arrow::Status statusNoPartitionTest = storage::DataWriter::WriteTable(*exampleTable, exampleTableName, outputFolder, noPartitioning);
+}
+
+int main() {
     std::string parquetFile = "test/NoPartition/test0.parquet";
     std::filesystem::path inputFile = std::filesystem::current_path().parent_path() / parquetFile;
-    arrow::Result<std::shared_ptr<arrow::Table>> tableFromDisk = storage::DataReader::ReadTable(inputFile).ValueOrDie();
+    if (!std::filesystem::exists(inputFile)){
+        generate_test_parquet_file();
+    }
+    arrow::Result<std::shared_ptr<arrow::Table>> tableFromDisk = storage::DataReader::ReadTable(inputFile);
 
     arrow::Result<std::shared_ptr<arrow::Table>> exampleTable = storage::DataWriter::GenerateExampleTable().ValueOrDie();
     arrow::Result<std::shared_ptr<arrow::Table>> kdTreeExampleTable = storage::DataWriter::GenerateKDTreeExampleTable().ValueOrDie();
