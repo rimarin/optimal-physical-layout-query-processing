3ff8fb21bd2953981d4675d7c65a997169a27194
Riccardo Marin <riccardomarin23@gmail.com>
2023-10-14 12:15:24 +0200

    feat: Add 2 floats to data generator
    Add two float field - temperature and humidity - to the example table generator.

--- src/storage/DataWriter.cpp
+++ src/storage/DataWriter.cpp
@@ -24,16 +24,29 @@ namespace storage {
         std::shared_ptr<arrow::Array> years;
         ARROW_ASSIGN_OR_RAISE(years, int32builder.Finish());
 
-        std::vector<std::shared_ptr<arrow::Array>> columns = {days, months, years};
+        arrow::DoubleBuilder doubleBuilder;
+        double_t temperature_raw[5] = {0.4, 43.32, 23.43, 24.33, 13.34};
+        ARROW_RETURN_NOT_OK(doubleBuilder.AppendValues(temperature_raw, 5));
+        std::shared_ptr<arrow::Array> temperature;
+        ARROW_ASSIGN_OR_RAISE(temperature, doubleBuilder.Finish());
 
-        std::shared_ptr<arrow::Field> field_day, field_month, field_year;
+        double_t humidity_raw[5] = {34, 23, 78, 48, 10};
+        ARROW_RETURN_NOT_OK(doubleBuilder.AppendValues(humidity_raw, 5));
+        std::shared_ptr<arrow::Array> humidity;
+        ARROW_ASSIGN_OR_RAISE(humidity, doubleBuilder.Finish());
+
+        std::vector<std::shared_ptr<arrow::Array>> columns = {days, months, years, temperature, humidity};
+
+        std::shared_ptr<arrow::Field> field_day, field_month, field_year, field_temperature, field_humidity;
         std::shared_ptr<arrow::Schema> schema;
 
         field_day = arrow::field("Day", arrow::int32());
         field_month = arrow::field("Month", arrow::int32());
         field_year = arrow::field("Year", arrow::int32());
+        field_temperature = arrow::field("Temperature", arrow::float64());
+        field_humidity = arrow::field("Humidity", arrow::float64());
 
-        schema = arrow::schema({field_day, field_month, field_year});
+        schema = arrow::schema({field_day, field_month, field_year, field_temperature, field_humidity});
 
         std::shared_ptr<arrow::Table> table;
         table = arrow::Table::Make(schema, columns);
@@ -45,6 +58,7 @@ namespace storage {
                                          std::filesystem::path &outputFolder,
                                          const std::shared_ptr<partitioning::MultiDimensionalPartitioning> &partitioningMethod) {
         auto partitions = partitioningMethod->partition(table).ValueOrDie();
+        auto debug = table->ToString();
         std::string outPath = outputFolder.string();
         for (int i = 0; i < partitions.size(); ++i) {
             std::string outFilename = outPath + filename + std::to_string(i) + ".parquet";
