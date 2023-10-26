3f387dd2b0e3463d426b9a1bc89ed48bd039921b
Riccardo Marin <riccardomarin23@gmail.com>
2023-10-26 15:34:40 +0200

    benchmark: taxi random workload generation
    Generate workload as done in Tsunami, output queries within selectivity range into sql file

--- benchmark/workload_taxi.py
+++ benchmark/workload_taxi.py
@@ -1,7 +1,7 @@
 import datetime
 import duckdb
-import itertools
 import os
+import random
 import re
 import requests
 
@@ -25,7 +25,7 @@ class TaxiWorkload(Workload):
     def generate_dataset(self, **params):
         self.start_year = params.get("start_year", self.start_year)
         self.end_year = params.get("end_year", self.end_year)
-        years = [str(year) for year in range(self.start_year, self.end_year+1)]
+        years = [str(year) for year in range(self.start_year, self.end_year + 1)]
         months = [str(month).zfill(2) for month in range(1, 13)]
 
         for year in years:
@@ -40,16 +40,23 @@ class TaxiWorkload(Workload):
         self.total_rows = len(duckdb.sql(f'SELECT * FROM read_parquet(\'{self.get_files_pattern()}\')'))
 
     def generate_queries(self):
-        start_date = datetime.date(2018, 1, 1)
-        end_date = datetime.date(2019, 12, 31)
+        """
+        Approach resembles that in Tsunami paper:
+        1. Generate uniform number from 1 to 6 to pick the columns
+        2. Uniformly pick a value within the range of each column
+        However, we do not need the first step, since our query templates are already designed to have an increasing
+        number of dimensions.
+        """
+        start_date = datetime.datetime(2018, 1, 1)
+        end_date = datetime.datetime(2019, 12, 31)
         value_ranges = {
-            'PULocationID': range(1, 265),
-            'DOLocationID': range(1, 265),
-            'tpep_pickup_datetime': [start_date + datetime.timedelta(n) for n in range(int((end_date - start_date).days))],
-            'tpep_dropoff_datetime': [start_date + datetime.timedelta(n) for n in range(int((end_date - start_date).days))],
-            'passenger_count': range(1, 8),
-            'fare_amount': range(0, 500),
-            'trip_distance': range(1, 50),
+            'PULocationID': [1, 265],
+            'DOLocationID': [1, 265],
+            'tpep_pickup_datetime': [start_date, end_date],
+            'tpep_dropoff_datetime': [start_date, end_date],
+            'passenger_count': [1, 8],
+            'fare_amount': [0, 500],
+            'trip_distance': [1, 50],
         }
         placeholders = {
             '1': 'PULocationID',
@@ -67,30 +74,39 @@ class TaxiWorkload(Workload):
             '13': 'trip_distance',
             '14': 'trip_distance'
         }
-        templates = [_ for _ in range(1, 7+1)]
+        templates = [_ for _ in range(4, 7 + 1)]
+        num_queries_per_template = 500
         for template in templates:
-            selectivities = [0.001, 0.01, 0.1, 1, 10]
-            tolerance = 0.2  # 20 % tolerance
             with open(os.path.join(self.get_queries_folder(), f'{str(template)}.sql'), 'r') as template_file:
                 query_template = template_file.read()
             from_clause = f'FROM read_parquet(\'{self.get_dataset_folder()}/{self.get_table_name()}*.parquet\')'
             query = re.sub('FROM?(.*?)WHERE', f'{from_clause} where', query_template, flags=re.DOTALL)
-            used_placeholders = re.findall('\':(\d)\'', query)
-            list_of_ranges = [value_ranges[placeholders[used_placeholder]] for used_placeholder in used_placeholders]
-            combinations = list(itertools.product(*list_of_ranges))
-            for combination in combinations:
+            used_placeholders = re.findall('\':(\d+)\'', query)
+            for i in range(num_queries_per_template):
+                values_to_use = []
+                for used_placeholder in used_placeholders:
+                    value_range = value_ranges[placeholders[used_placeholder]]
+                    range_start, range_end = value_range[0], value_range[1]
+                    if isinstance(range_start, datetime.datetime):
+                        random_value = ("'" + (range_start + (range_end - range_start) * random.random())
+                                        .strftime("%Y%m%d%H:%M:%S") + "'")
+                    else:
+                        random_value = random.uniform(range_start, range_end)
+                    values_to_use.append(random_value)
                 final_query = query[:]
-                for i in range(1, len(combination)+1):
-                    final_query = re.sub(f'\':{i}\'', f'{combination[i-1]}', final_query, flags=re.DOTALL)
+                for i in range(1, len(values_to_use) + 1):
+                    final_query = re.sub(f'\':{i}\'', f'{values_to_use[i - 1]}', final_query, flags=re.DOTALL)
                 print(f'Executing query: {final_query}')
-                num_tuples = len(duckdb.sql(final_query))
-                query_selectivity = num_tuples / self.get_total_rows()
-                for selectivity in selectivities:
-                    if (selectivity - (selectivity * tolerance)) < query_selectivity < (selectivity + (selectivity * tolerance)):
-                        print(f"Found a valid query candidate! {final_query}")
-                        with open(self.get_queries_folder() + f'{str(template)}_{str(query_selectivity)}.sql', 'w') as query_file:
-                            query_file.write(final_query)
-                        selectivities.remove(selectivity)
+                try:
+                    num_tuples = len(duckdb.sql(final_query))
+                except Exception as e:
+                    print(f"Error {str(e)} while executing query: {final_query}")
+                    continue
+                query_selectivity = (num_tuples / self.get_total_rows()) * 100
+                if query_selectivity != 0 and 0.001 < query_selectivity < 10:
+                    with open(os.path.join(self.get_generated_queries_folder(), f'{str(template)}_{str(query_selectivity)}.sql'),
+                              'w') as query_file:
+                        query_file.write(final_query)
 
     def is_dataset_generated(self) -> bool:
         first_file = os.path.join(self.get_dataset_folder(), self.get_table_name() + f'_{self.start_year}-01.parquet')
