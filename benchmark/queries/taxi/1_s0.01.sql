abc920e563f6d6b55849cdb31c94efbb1e41a242
Riccardo Marin <riccardomarin23@gmail.com>
2023-10-30 12:12:26 +0100

    refactor: remove obsolete generated queries
    Do not use queries generated manually for tpch

--- benchmark/queries/taxi/1_s0.01.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-FROM read_parquet('datasets/taxi/*.parquet')
-WHERE PULocationID > 120 AND PULocationID < 130
--- benchmark/queries/taxi/1_s9.73.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-FROM read_parquet('datasets/taxi/*.parquet')
-WHERE PULocationID > 13 AND PULocationID < 70
