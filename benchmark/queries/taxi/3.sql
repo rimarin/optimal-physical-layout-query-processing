670952f7ca184abb8d7ab91834ff814381408468
Riccardo Marin <riccardomarin23@gmail.com>
2023-12-23 09:09:39 +0100

    refactor: query templates formatting
    New line.

--- benchmark/queries/taxi/3.sql
+++ benchmark/queries/taxi/3.sql
@@ -2,6 +2,5 @@
 SELECT *
 FROM read_parquet('datasets/taxi/*.parquet')
 WHERE PULocationID > ':1' AND PULocationID < ':2' AND
-    DOLocationID > ':3' AND DOLocationID < ':4'
-AND tpep_pickup_datetime > ':5' AND
-    tpep_pickup_datetime < ':6'
+      DOLocationID > ':3' AND DOLocationID < ':4' AND
+      tpep_pickup_datetime > ':5' AND tpep_pickup_datetime < ':6'
--- benchmark/queries/taxi/4.sql
+++ benchmark/queries/taxi/4.sql
@@ -2,8 +2,6 @@
 SELECT *
 FROM read_parquet('datasets/taxi/*.parquet')
 WHERE PULocationID > ':1' AND PULocationID < ':2' AND
-      DOLocationID > ':3' AND DOLocationID < ':4'
-AND tpep_pickup_datetime > ':5' AND
-    tpep_pickup_datetime < ':6' AND
-    tpep_dropoff_datetime > ':7' AND
-    tpep_dropoff_datetime < ':8'
+      DOLocationID > ':3' AND DOLocationID < ':4' AND
+      tpep_pickup_datetime > ':5' AND tpep_pickup_datetime < ':6' AND
+      tpep_dropoff_datetime > ':7' AND tpep_dropoff_datetime < ':8'
