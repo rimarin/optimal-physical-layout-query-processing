e13208fe0571d7a76f1763dcd67d565dfd90e12e
Riccardo Marin <riccardomarin23@gmail.com>
2023-12-24 16:59:08 +0100

    feat: data reader wrapper
    batch read support, API for accessing parquet metadata, extract columns.

--- libpartitioner/include/storage/DataReader.h
+++ libpartitioner/include/storage/DataReader.h
@@ -1,5 +1,5 @@
-#ifndef STORAGE_DATAREADER_H
-#define STORAGE_DATAREADER_H
+#ifndef STORAGE_DATA_READER_H
+#define STORAGE_DATA_READER_H
 
 #include <filesystem>
 
@@ -18,13 +18,27 @@ class DataReader {
     public:
         DataReader() = default;
         virtual ~DataReader() = default;
-        // Load entire table into memory
-        static arrow::Result<std::shared_ptr<arrow::Table>> readTable(std::filesystem::path &inputFile);
-        // Read table batch by batch
-        static arrow::Result<std::shared_ptr<::arrow::RecordBatchReader>> getTableBatchReader(std::filesystem::path &inputFile);
+        arrow::Status load(std::filesystem::path &filePath, bool useBatchRead = false);
+        arrow::Result<std::shared_ptr<arrow::Table>> readTable();
+        arrow::Result<std::shared_ptr<::arrow::RecordBatchReader>> getTableBatchReader();
         static arrow::Result<std::vector<std::shared_ptr<arrow::Array>>> getColumns(std::shared_ptr<arrow::Table> &table,
                                                                                     std::vector<std::string> &columns);
+        arrow::Result<std::vector<std::shared_ptr<arrow::ChunkedArray>>> getColumns(const std::vector<std::string> &columns);
+        void displayFileProperties();
+        arrow::Result<std::pair<uint64_t, uint64_t>> getColumnStats(const std::string &columnName);
+        uint64_t getNumRows();
+        static arrow::Result<std::shared_ptr<arrow::Table>> getTable(std::filesystem::path &inputFile);
+        static std::filesystem::path getDatasetPath(const std::filesystem::path &folder, const std::string &datasetName,
+                                                    const std::string &partitioningTechnique);
+    private:
+        std::filesystem::path path;
+        bool batchRead = false;
+        bool isFolder = false;
+        std::unique_ptr<parquet::arrow::FileReader> reader;
+        std::shared_ptr<parquet::FileMetaData> metadata;
+        arrow::Result<std::shared_ptr<arrow::ChunkedArray>> getColumn(const std::string &columnName);
+        arrow::Result<int> getColumnIndex(const std::string &columnName);
 };
 } // storage
 
-#endif //STORAGE_DATAREADER_H
+#endif //STORAGE_DATA_READER_H
--- libpartitioner/storage/DataReader.cpp
+++ libpartitioner/storage/DataReader.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include <filesystem>
-#include <arrow/filesystem/api.h>
-#include <arrow/filesystem/localfs.h>
+#include <iostream>
 #include <string>
+
+#include <arrow/filesystem/localfs.h>
 #include <arrow/io/api.h>
 #include <parquet/arrow/reader.h>
 
@@ -10,43 +10,55 @@
 
 namespace storage {
 
-    void exploreDataset(std::filesystem::path &base_dir){
-        // Create a filesystem
-        std::shared_ptr<arrow::fs::LocalFileSystem> fs = std::make_shared<arrow::fs::LocalFileSystem>();
+    arrow::Status DataReader::load(std::filesystem::path &filePath, bool useBatchRead){
+        path = filePath;
+        batchRead = useBatchRead;
+        if (std::filesystem::path(path).has_filename()){
+            isFolder = false;
 
-        // Create a file selector which describes which files are part of
-        // the dataset.  This selector performs a recursive search of a base
-        // directory which is typical with partitioned datasets.  You can also
-        // create a dataset from a list of one or more paths.
-        arrow::fs::FileSelector selector;
-        selector.base_dir = base_dir;
-        selector.recursive = true;
+            arrow::MemoryPool* pool = arrow::default_memory_pool();
 
-        // List out the files, so we can see how our data is partitioned.
-        // This step is not necessary for reading a dataset
-        std::vector<arrow::fs::FileInfo> file_infos = fs->GetFileInfo(selector).ValueOrDie();
-        int num_printed = 0;
-        for (const auto& path : file_infos) {
-            if (path.IsFile()) {
-                std::cout << path.path().substr(base_dir.string().size()) << std::endl;
-                if (++num_printed == 10) {
-                    std:: cout << "..." << std::endl;
-                    break;
-                }
-            }
+            // Enable parallel column decoding
+            auto reader_properties = parquet::ReaderProperties(pool);
+
+            reader_properties.set_buffer_size(4096 * 4);
+            reader_properties.enable_buffered_stream();
+
+            // Configure Arrow-specific Parquet reader settings
+            auto arrow_reader_props = parquet::ArrowReaderProperties(/*use_threads=*/true);
+            arrow_reader_props.set_batch_size(128 * 1024);  // default 64 * 1024
+
+            parquet::arrow::FileReaderBuilder reader_builder;
+            ARROW_RETURN_NOT_OK(reader_builder.OpenFile(path, /*memory_map=*/false, reader_properties));
+            reader_builder.memory_pool(pool);
+            reader_builder.properties(arrow_reader_props);
+
+            ARROW_ASSIGN_OR_RAISE(reader, reader_builder.Build());
+
+            std::cout << "[DataReader] Loaded file reader for file " << path << std::endl;
+
+            metadata = reader->parquet_reader()->metadata();
+
+            displayFileProperties();
+
+            return arrow::Status::OK();
+        }
+        else{
+            return arrow::Status::Invalid("Multi-file read not supported for now");
         }
     }
 
-    uint64_t getTotalRowNumber(std::string& filePath) {
-        std::shared_ptr<arrow::io::ReadableFile> infile;
-        PARQUET_ASSIGN_OR_THROW(infile, arrow::io::ReadableFile::Open(filePath, arrow::default_memory_pool()));
-        std::unique_ptr<parquet::arrow::FileReader> reader;
-        PARQUET_THROW_NOT_OK(parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &reader));
-        std::shared_ptr<parquet::FileMetaData> metadata = reader->parquet_reader()->metadata();
+    void DataReader::displayFileProperties(){
+        std::cout << "[DataReader] File has " << metadata->num_columns() << " columns" << std::endl;
+        std::cout << "[DataReader] File has " << metadata->num_row_groups() << " row groups" << std::endl;
+        std::cout << "[DataReader] File has " << metadata->num_rows() << " rows" << std::endl;
+    }
+
+    uint64_t DataReader::getNumRows() {
         return metadata->num_rows();
     }
 
-    arrow::Result<std::shared_ptr<arrow::Table>> DataReader::readTable(std::filesystem::path &inputFile) {
+    arrow::Result<std::shared_ptr<arrow::Table>> DataReader::getTable(std::filesystem::path &inputFile){
         arrow::MemoryPool* pool = arrow::default_memory_pool();
         std::shared_ptr<arrow::io::RandomAccessFile> input;
         ARROW_ASSIGN_OR_RAISE(input, arrow::io::ReadableFile::Open(inputFile));
@@ -60,33 +72,24 @@ namespace storage {
         return table;
     }
 
+    arrow::Result<std::shared_ptr<arrow::Table>> DataReader::readTable() {
+        std::shared_ptr<arrow::Table> table;
+        ARROW_RETURN_NOT_OK(reader->ReadTable(&table));
+        std::cout << "[DataReader] Read entire table" << std::endl;
+        return table;
+    }
+
+    std::filesystem::path DataReader::getDatasetPath(const std::filesystem::path &folder, const std::string &datasetName,
+                                         const std::string &partitioningTechnique){
+        return folder / datasetName / partitioningTechnique / (datasetName + ".parquet");
+    }
+
     // TODO: reduce memory usage, see:
     //  https://github.com/lsst/qserv/blob/a5dbf4175159b874da1cb0907533ba6e3ffd5e7d/src/partition/ParquetInterface.cc#L114
-    arrow::Result<std::shared_ptr<::arrow::RecordBatchReader>> DataReader::getTableBatchReader(std::filesystem::path &inputFile) {
-        arrow::MemoryPool* pool = arrow::default_memory_pool();
-
-        // Enable parallel column decoding
-        auto reader_properties = parquet::ReaderProperties(pool);
-
-        reader_properties.set_buffer_size(4096 * 4);
-        reader_properties.enable_buffered_stream();
-
-        // Configure Arrow-specific Parquet reader settings
-        auto arrow_reader_props = parquet::ArrowReaderProperties(/*use_threads=*/true);
-        arrow_reader_props.set_batch_size(128 * 1024);  // default 64 * 1024
-
-        parquet::arrow::FileReaderBuilder reader_builder;
-        ARROW_RETURN_NOT_OK(
-                reader_builder.OpenFile(inputFile, /*memory_map=*/false, reader_properties));
-        reader_builder.memory_pool(pool);
-        reader_builder.properties(arrow_reader_props);
-
-        std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
-        ARROW_ASSIGN_OR_RAISE(arrow_reader, reader_builder.Build());
-
+    arrow::Result<std::shared_ptr<::arrow::RecordBatchReader>> DataReader::getTableBatchReader() {
         std::shared_ptr<arrow::RecordBatchReader> rb_reader;
-        ARROW_RETURN_NOT_OK(arrow_reader->GetRecordBatchReader(&rb_reader));
-
+        ARROW_RETURN_NOT_OK(reader->GetRecordBatchReader(&rb_reader));
+        std::cout << "[DataReader] Obtained record batch reader" << std::endl;
         return rb_reader;
     }
 
@@ -112,6 +115,49 @@ namespace storage {
             columnData.emplace_back(chunk);
         }
         return columnData;
+    }
+
+    arrow::Result<std::vector<std::shared_ptr<arrow::ChunkedArray>>> DataReader::getColumns(const std::vector<std::string> &columns){
+        std::vector<std::shared_ptr<arrow::ChunkedArray>> columnsArray;
+        columnsArray.reserve(columns.size());
+        for (const auto &column: columns){
+            columnsArray.emplace_back(getColumn(column).ValueOrDie());
+        }
+        return columnsArray;
+    }
+
+    arrow::Result<std::shared_ptr<arrow::ChunkedArray>> DataReader::getColumn(const std::string &columnName){
+        std::cout << "Reading first column of parquet-arrow-example.parquet" << std::endl;
+        std::shared_ptr<arrow::ChunkedArray> columnArray;
+        auto fieldIndex = getColumnIndex(columnName).ValueOrDie();
+        if (fieldIndex == -1){
+            return arrow::Status::Invalid("Column name <" + columnName + "> not found in schema");
+        }
+        PARQUET_THROW_NOT_OK(reader->ReadColumn(fieldIndex, &columnArray));
+        return columnArray;
+    }
+
+    arrow::Result<int> DataReader::getColumnIndex(const std::string &columnName){
+        std::shared_ptr<arrow::Schema> schema;
+        ARROW_RETURN_NOT_OK(reader->GetSchema(&schema));
+        return schema->GetFieldIndex(columnName);
+    }
+
+    arrow::Result<std::pair<uint64_t, uint64_t>> DataReader::getColumnStats(const std::string &columnName){
+        auto numRowGroups = metadata->num_row_groups();
+        std::unique_ptr<parquet::RowGroupMetaData> rowGroup = metadata->RowGroup(0);
+        std::shared_ptr<parquet::Statistics> stats = (rowGroup->ColumnChunk(getColumnIndex(columnName).ValueOrDie()))->statistics();
+        const auto *typed_stats = dynamic_cast<const parquet::TypedStatistics<arrow::Int64Type>*>(stats.get());
+        auto minValue = typed_stats->min();
+        auto maxValue = typed_stats->max();
+        for (int i = 1; i < numRowGroups; ++i){
+            rowGroup = metadata->RowGroup(i);
+            stats = (rowGroup->ColumnChunk(0))->statistics();
+            typed_stats = dynamic_cast<const parquet::TypedStatistics<arrow::Int64Type>*>(stats.get());
+            minValue = std::min(minValue, typed_stats->min());
+            maxValue = std::max(maxValue, typed_stats->max());
+        }
+        return std::make_pair(minValue, maxValue);
     }
 
 } // storage
