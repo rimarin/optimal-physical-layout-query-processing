00655777c4a1cd93d6b23a8ba6699724c9c10027
Riccardo Marin <riccardomarin23@gmail.com>
2023-12-29 18:43:09 +0100

    fix: dataset folder from args

--- partitioner/partitioner.cpp
+++ partitioner/partitioner.cpp
@@ -6,29 +6,34 @@
 #include "experimentsConfig.cpp"
 
 int main(int argc, char **argv) {
-    if (argc < 5){
-        std::cout << "Insufficient number of arguments\n";
-        std::cout << "Expected syntax: partitioner <dataset_name> <partitioning_technique> <partition_size> <columns>\n";
+    if (argc < 6){
+        std::cout << "Insufficient number of arguments\n" << std::endl;
+        std::cout << "Expected syntax: partitioner <dataset_base_path> <dataset_name> <partitioning_technique> <partition_size> <columns>\n" << std::endl;
         exit(1);
     }
-    std::string argDatasetName = argv[1];
-    if (ExperimentsConfig::realDatasets.find(argv[1]) == ExperimentsConfig::realDatasets.end()){
-        std::cout << "Dataset not available/recognized";
+    std::filesystem::path argDatasetPath(argv[1]);
+    if (!std::filesystem::exists(argDatasetPath)){
+        std::cout << "Dataset folder " << argDatasetPath << " does not exist" << std::endl;
         exit(1);
     }
-    std::string argPartitioningTechnique = argv[2];
+    std::string argDatasetName = argv[2];
+    if (ExperimentsConfig::realDatasets.find(argDatasetName) == ExperimentsConfig::realDatasets.end()){
+        std::cout << "Dataset not available/recognized" << std::endl;
+        exit(1);
+    }
+    std::string argPartitioningTechnique = argv[3];
     std::set<std::string> partitioningTechniques = {};
     for (const auto &item: ExperimentsConfig::nameToPartitioningTechnique){
         partitioningTechniques.insert(item.first);
     }
     if (ExperimentsConfig::nameToPartitioningTechnique.find(argPartitioningTechnique) ==
     ExperimentsConfig::nameToPartitioningTechnique.end()){
-        std::cout << "Partitioning technique not available/recognized";
+        std::cout << "Partitioning technique not available/recognized" << std::endl;
         exit(1);
     }
-    std::string argPartitionSize = argv[3];
+    std::string argPartitionSize = argv[4];
     auto partitionSize = std::stoi(argPartitionSize);
-    std::string argColumns = argv[4];
+    std::string argColumns = argv[5];
     std::vector<std::string> partitioningColumns;
     std::string segment;
     std::stringstream ss(argColumns);
@@ -37,15 +42,12 @@ int main(int argc, char **argv) {
         partitioningColumns.push_back(segment);
     }
 
-    std::filesystem::path projectPath = std::filesystem::current_path().parent_path().parent_path();
-    std::filesystem::path datasetPath = projectPath / "benchmark" / "datasets" / argDatasetName;
-
-    std::filesystem::path datasetFilePath = datasetPath / ExperimentsConfig::noPartition / (argDatasetName + ".parquet");
-    if (!std::filesystem::exists(datasetPath)){
-        std::cout << "Dataset not found in " << datasetPath;
+    std::filesystem::path datasetFilePath = argDatasetPath / ExperimentsConfig::noPartition / (argDatasetName + ".parquet");
+    if (!std::filesystem::exists(datasetFilePath)){
+        std::cout << "Not partitioned source dataset not found in " << datasetFilePath << std::endl;
         exit(1);
     }
-    std::filesystem::path outputPath = datasetPath / argPartitioningTechnique;
+    std::filesystem::path outputPath = argDatasetPath / argPartitioningTechnique;
     storage::DataWriter::cleanUpFolder(outputPath);
     auto dataReader = storage::DataReader();
     std::ignore = dataReader.load(datasetFilePath);
