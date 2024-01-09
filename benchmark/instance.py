e0850cdfbcc85f9cdcefe1710fec0bdbe758c020
Riccardo Marin <riccardomarin23@gmail.com>
2024-01-09 22:33:28 +0100

    fix: hot fix python 3.6 vs python 3.7 subprocess

--- benchmark/instance.py
+++ benchmark/instance.py
@@ -144,6 +144,8 @@ class BenchmarkInstance:
                     if process.returncode != 0:
                         self.logger.error(f"Received return code {str(process.returncode)}")
                     process_output = process.stderr.decode("utf-8")
+                    if len(str(process_output).split('\n')) < 2:
+                        raise Exception
                 except Exception as e:
                     self.logger.error(f"Error while calling benchmark runner, {str(e)}")
                     if i < max_retries - 1:  # i is zero indexed
