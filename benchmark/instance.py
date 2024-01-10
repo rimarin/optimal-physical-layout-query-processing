99732705af932aeec2955325168b68f6cc6d0991
Riccardo Marin <riccardomarin23@gmail.com>
2024-01-10 19:42:34 +0100

    fix: add timeout, process is still hanging sometimes

--- benchmark/instance.py
+++ benchmark/instance.py
@@ -144,7 +144,7 @@ class BenchmarkInstance:
                     self.logger.info("Launching benchmarks...")
                     cmd = [f'{self.duckdb_path}/build/release/benchmark/benchmark_runner', 'PartitioningBenchmark',
                            f'--out={RESULTS_LOG_FILE}']
-                    process = subprocess.run(cmd)
+                    process = subprocess.run(cmd, timeout=300)
                     if process.returncode != 0:
                         self.logger.error(f"Received return code {str(process.returncode)}")
                         raise Exception("Benchmark did not succeed")
