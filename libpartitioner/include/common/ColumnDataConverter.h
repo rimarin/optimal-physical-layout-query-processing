7d6dd18984dda5545049c16f9366aba847ee01d2
Riccardo Marin <riccardomarin23@gmail.com>
2024-01-13 20:14:20 +0100

    feat: handle timestamp datatype

--- libpartitioner/include/common/ColumnDataConverter.h
+++ libpartitioner/include/common/ColumnDataConverter.h
@@ -194,6 +194,29 @@ namespace common {
             return arrow::Status::OK();
         }
 
+        // From timestamp conversion
+        template<typename ArrayType, typename DataType = typename ArrayType::TypeClass, typename CType = typename DataType::c_type>
+        arrow::enable_if_timestamp<DataType, arrow::Status> Visit(const ArrayType &array) {
+            auto castedArray = std::make_shared<std::vector<double>>();
+            for (std::optional<CType> value: array) {
+                if (value.has_value()) {
+                    if (outputType == "int32"){
+                        castedArray->emplace_back(static_cast<int32_t>(value.value()));
+                    }
+                    else if (outputType == "int64"){
+                        castedArray->emplace_back(static_cast<int64_t>(value.value()));
+                    }
+                    else{
+                        castedArray->emplace_back(static_cast<double>(value.value()));
+                    }
+                }
+            }
+            if (!castedArray->empty()) {
+                convertedData.emplace_back(castedArray);
+            }
+            return arrow::Status::OK();
+        }
+
     private:
         std::vector<std::shared_ptr<common::Point>> convertedData = {};
         std::string outputType = "double";
