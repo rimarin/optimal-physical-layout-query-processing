33e7283f90411f725538f12a3a965f4eb90b0e48
Riccardo Marin <riccardomarin23@gmail.com>
2024-01-22 15:28:48 +0100

    feat: tests for quad tree partitioning

--- test/testPartitionTPCH.cpp
+++ test/testPartitionTPCH.cpp
@@ -29,6 +29,30 @@ TEST_F(TestOptimalLayoutFixture, TestPartitioningFixedGridTPCH){
     ASSERT_EQ(numTotalRows, partitionsTotalRows);
 }
 
+TEST_F(TestOptimalLayoutFixture, TestPartitioningQuadTreeTPCH){
+    GTEST_SKIP();
+    auto folder = ExperimentsConfig::quadTreeFolder;
+    auto dataset = getDatasetPath(ExperimentsConfig::datasetTPCH1);
+    auto partitionSize = 20000;
+    auto numTotalRows = 239917;
+    cleanUpFolder(folder);
+    std::vector<std::string> partitioningColumns = {"c_custkey", "l_orderkey"};
+    auto dataReader = std::make_shared<storage::DataReader>();
+    ASSERT_EQ(dataReader->load(dataset), arrow::Status::OK());
+    ASSERT_EQ(dataReader->getNumRows(), numTotalRows);
+    auto partitioning = partitioning::PartitioningFactory::create(partitioning::STR_TREE, dataReader, partitioningColumns, partitionSize, folder);
+    ASSERT_EQ(partitioning->partition(), arrow::Status::OK());
+    std::filesystem::path partition0 = folder / "0.parquet";
+    ASSERT_EQ(dataReader->load(partition0), arrow::Status::OK());
+    std::filesystem::path partition1 = folder / "13.parquet";
+    ASSERT_EQ(dataReader->load(partition1), arrow::Status::OK());
+    auto folderResults = getFolderResults(dataReader, folder);
+    auto fileCount = folderResults.first;
+    auto partitionsTotalRows = folderResults.second;
+    ASSERT_EQ(fileCount, 28);
+    ASSERT_EQ(numTotalRows, partitionsTotalRows);
+}
+
 TEST_F(TestOptimalLayoutFixture, TestPartitioningSTRTreeTPCH){
     GTEST_SKIP();
     auto folder = ExperimentsConfig::strTreeFolder;
