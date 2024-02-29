43d089b9e9b826e44d45f478d83fac490d40a090
Riccardo Marin <riccardomarin23@gmail.com>
2024-02-29 15:31:13 +0100

    fix: update timeout
    Should allow up to 1 min per query, for 5 queries in total

--- benchmark/instance.py
+++ benchmark/instance.py
@@ -154,7 +154,7 @@ class BenchmarkInstance:
                     self.logger.info("Launching benchmarks...")
                     cmd = [f'{self.duckdb_path}/build/release/benchmark/benchmark_runner', 'PartitioningBenchmark',
                            f'--out={RESULTS_LOG_FILE}']
-                    timeout = 180  # s
+                    timeout = 300  # s
                     process = subprocess.run(cmd, timeout=timeout)
                     if process.returncode != 0:
                         self.logger.warning(f"Received return code {str(process.returncode)} from benchmark runner")
